52f1e880e03eb5a5551a492c5113d87ffbac6209
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-04-24 03:00:59 +0000

    code

--- module2/index.js
+++ module2/index.js
@@ -4,4 +4,20 @@ app.listen(3000, () => {
   console.log('listening 3000')
 })
 
+const dataBase = []
 app.use(express.static("public"))
+app.use(express.json({ limit: "1mb" }))
+
+
+app.post("/api", (request, response) => {
+  console.log("i got a request")
+  console.log(request.body)
+  const data = request.body
+  dataBase.push(data)
+  response.json({
+    status: "success",
+    latitude: data.lat,
+    longitude: data.lon,
+    dataB: {dataBase}
+   })
+})
